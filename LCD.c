72f3d8ae18665086b984feb45a242b3fb571a389
Khalid Soliman Ali <khaled_solyman2000@yahoo.com>
2018-02-22 03:17:20 +0200

    Add Some Functions

--- LCD.c
+++ LCD.c
@@ -20,6 +20,22 @@ void LCD_EN()
 	_delay_us(5);
 	clear_bit(PORTC,7);
 }
+void LCD_init()
+{
+	DDRB=0xff; // Make PORT output
+	PORTB=0;   // Init PORT
+	////////////////////////////////////////
+	DDRC|=(1<<6);   // Make RS Output
+	PORTC&=~(1<<6); // Init RS " Command mode "
+	DDRC|=(1<<7);   // Make EN Output
+	PORTC&=~(1<<7); // Init EN
+
+	LCD_Write_CMD(0x01); // Clear All Display Data
+	LCD_Write_CMD(0x28); // 4 bit mode 5*8 dispaly  2 line
+	LCD_Write_CMD(0x02); // Return Home
+	LCD_Write_CMD(0x0E); // Display on-Cursor on-Blink off
+	LCD_Write_CMD(0x06); // after every character cursor will move to right
+}
 void LCD_Write_CMD(unsigned char CMD)
 {
 	clear_bit(PORTC,6);
@@ -29,9 +45,8 @@ void LCD_Write_CMD(unsigned char CMD)
 	PORTB&=0x0F;
 	PORTB |=((CMD<<4)&0xF0);
 	LCD_EN();
-	_delay_ms(10);
 	if (CMD==0x01)	curcor_counter = 0;
-
+	_delay_ms(10);
 }
 void Toggle_Curcor_Pos()
 {
@@ -49,24 +64,6 @@ void Toggle_Curcor_Pos()
 		toggle_counter=0;
 	}
 }
-void LCD_init()
-{
-	DDRB=0xff; // Make PORT output
-	PORTB=0;   // Init PORT
-	////////////////////////////////////////
-	DDRC|=(1<<6);   // Make RS Output
-	PORTC&=~(1<<6); // Init RS " Command mode "
-	DDRC|=(1<<7);   // Make EN Output
-	PORTC&=~(1<<7); // Init EN
-
-	LCD_Write_CMD(0x01); // Clear All Display Data
-	LCD_Write_CMD(0x28); // 4 bit mode 5*8 dispaly  2 line
-	LCD_Write_CMD(0x02); // Return Home
-	LCD_Write_CMD(0x0E); // Display on-Cursor on-Blink off
-	LCD_Write_CMD(0x06); // after every character cursor will move to right
-}
-
-
 
 void LCD_Write_Data (unsigned char Data)
 {
@@ -89,10 +86,17 @@ void LCD_Write_String(char *string)
 		string ++;
 	}
 }
-
-
 void LCD_GoTo(unsigned char posX,unsigned char posY)
 {
 	unsigned char curcor_pos[] = {0x80,0xC0};
 	LCD_Write_CMD(curcor_pos[posX-1]+posY-1);
+}
+void LCD_Send_String_Row_Column(char Row,char Column,char *String)
+{
+	LCD_GoTo(Row,Column);
+	LCD_Write_String(String);
+}
+void LCD_Int_2_String(int x , char* string)
+{
+	sprintf(string, "%d", x);
 }
