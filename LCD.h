210d8fd0b7035e35248e3b3ada9c6ae621364244
Khalid Soliman Ali <khaled_solyman2000@yahoo.com>
2018-02-22 03:17:48 +0200

    Add Some Functions

--- LCD.h
+++ LCD.h
@@ -15,6 +15,8 @@ void LCD_EN();
 void Toggle_Curcor_Pos();
 void LCD_GoTo(unsigned char posX,unsigned char posY);
 void LCD_Write_String( char *string);
+void LCD_Send_String_Row_Column(char Row,char Column,char *String);
+void LCD_Int_2_String(int x , char* string);
 
 
 #endif /* LCD_LCD_H_ */
